d4bb186c00277c7157dca02d7693c347a30b45be
Davi <davi.rosim@ges.inatel.br>
2025-07-24 14:23:18 -0300

    fix: wrong imports

--- src/vss_env/clients/sim/__init__.py
+++ src/vss_env/clients/sim/__init__.py
@@ -1,3 +1,3 @@
-from src.vss_env.clients.sim.actuator import ActuatorClient
-from src.vss_env.clients.sim.vision import VisionClient
-from src.vss_env.clients.sim.replacer import ReplacerClient
+from vss_env.clients.sim.actuator import ActuatorClient
+from vss_env.clients.sim.vision import VisionClient
+from vss_env.clients.sim.replacer import ReplacerClient
